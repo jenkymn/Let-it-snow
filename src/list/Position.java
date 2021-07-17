1897903550d1efe709753ac37eab8b8a1707a8c0
jenkymn <87531839+jenkymn@users.noreply.github.com>
2021-07-17 16:34:47 +0800

    Create Position.java

---
+++ src/list/Position.java
@@ -0,0 +1,21 @@
+package lis;
+
+
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int xpos,int ypos){
+        this.x=xpos;
+        this.y=ypos;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+}
